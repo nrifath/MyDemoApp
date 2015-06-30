71ac82ce6ff3603ec5a3c3387747205e5b52fa42
nrifath <nrifath@yahoo.com>
2015-06-30 14:45:28 +0600

    add department class

---
+++ DemoApp/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoApp
+{
+    public class Department
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+    }
+}
